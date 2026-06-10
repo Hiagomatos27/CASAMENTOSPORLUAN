495184b44b15f307cd72da741431873b1a6ca6a2
Hiagomatos27 <hiagomatos1224@gmail.com>
2026-06-10 00:50:59 -0300

    final 01

---
+++ src/server.ts
@@ -0,0 +1,3 @@
+import { createMiddleware, createStart } from "@tanstack/react-start/server";
+
+export default createStart();
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+
+export default defineConfig({
+  plugins: [
+    tanstackStart({
+      target: "vercel",
+      server: { entry: "src/server.ts" },
+    }),
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
+});
